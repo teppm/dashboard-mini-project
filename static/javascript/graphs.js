4361a4878eb26b8d3e5e15d23a88b38dbd9f9349
Tepp <Madis.Tepp@sas.se>
2019-11-08 10:34:50 +0100

    need to fix data loading

--- static/javascript/graphs.js
+++ static/javascript/graphs.js
@@ -3,6 +3,29 @@ queue()
     .await(makeGraphs);
 
 function makeGraphs(error, salaryData) {
+    var ndx = crossfilter(salaryData);
 
+    show_gender_balance(ndx);
 
-}
+    dc.renderAll();
+};
+
+function show_gender_balance(ndx) {
+
+    var dim = ndx.dimension(dc.pluck('sex'))
+    var group = dim.group();
+
+    dc.barchart('#gender-balance')
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel('Gender')
+        .yAxis().ticks(20);
+
+};
